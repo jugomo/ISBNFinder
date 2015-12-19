0ea686846443e3e224874299f6fb07de94349f67
jugomo <julio@MacBook-de-julio.local>
2015-12-19 14:30:29 +0100

    supported no internet connection error
    added control of no internet errors.
    added app icon.

--- ISBNFinder/ViewController.swift
+++ ISBNFinder/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     let urlBase = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
     
     @IBOutlet weak var isbnInput: UITextField!
@@ -37,29 +37,45 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         isbnInput.delegate = self
     }
-
+    
+    /**
+     launch synchronous request
+     */
     func sincrona(isbn: String) {
         let url = NSURL(string: urlBase + isbn)
         let data = NSData(contentsOfURL: url!)
         let response = NSString(data: data!, encoding: NSUTF8StringEncoding)
-        print("SINCRONA: " + (response! as String))
+        
+        // view response text
         verRespuesta(response)
     }
     
+    /**
+     launch asynchronous request
+     */
     func asincrona(isbn: String) {
         let url = NSURL(string: urlBase + isbn)
         let session = NSURLSession.sharedSession()
+        
+        // set handler to manage request
         let handler = {
             (datos: NSData?, resp: NSURLResponse?, error: NSError?) -> Void in
+            var texto: NSString? = nil
             
-            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-            print("ASINCRONA: " + (texto! as String))
+            if resp != nil && datos?.length > 0 {
+                // convert response to text
+                texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            } else {
+                texto = "Ha habido algún error con la conexión."
+            }
             
+            // view response text into main thread
             dispatch_async(dispatch_get_main_queue()) {
                 self.verRespuesta(texto)
             }
         }
         
+        // execute request
         let dt = session.dataTaskWithURL(url!, completionHandler: handler)
         dt.resume()
     }
@@ -70,7 +86,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         if respuesta != nil && respuesta != "{}" {
             bookOutput.text = respuesta! as String
         } else {
-            bookOutput.text = "Se ha producido un error al realizar su petición."
+            bookOutput.text = "Su petición no ha sido procesada correctamente en el servidor."
         }
     }
 }
